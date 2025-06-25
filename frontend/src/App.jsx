ec63043d061bc821359407e12c6c02108404d851
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-06-25 10:24:21 +0300

    Configure proxy in vite.config.js and add handleSubmit function with axios.post for task submission

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,58 @@
+import React from 'react'
 import { useState } from 'react'
+import axios from 'axios';
+
 
 function App() {
+  const [newTask, setNewTask] = useState('')
+  
+  const [tasks, setTasks] = useState([])
 
-  return (
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!newTask.trim()) return;
+   try {
+    const response = await axios.post('/api/todos', {
+      title: newTask
+    });
+    setTasks([...tasks, response.data]);
+    setNewTask(''); 
+
+    console.log("Task added:", response.data);
+    
+   } catch (error) {
+    console.error("Error adding task:", error);
+    
+   }
+
+  }
+
+
+   return (
     <>
-  <h1> Hello todo</h1>
+    <div>
+      <div>
+        <h1>Task Manager</h1>
+        <form onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          placeholder="Enter a task"
+          value={newTask}
+          onChange={(e)=> setNewTask(e.target.value)}
+          required
+           />
+
+          <button 
+          type="submit"
+          
+          >Add Task</button>
+        </form>
+
+      </div>
+    </div>
     </>
   )
 }
 
 export default App
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000', // Adjust the target to your backend server
+      },
+    },
+  },
 })
